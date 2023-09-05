8522858b71fd0704b402dac04f0b03dae873a0e7
cornelcristea <petrucornel.cristea@gmail.com>
2023-09-05 10:17:24 +0300

    update test script

--- main/test/main_test.py
+++ main/test/main_test.py
@@ -1,41 +1,42 @@
 
 import unittest
-from os import mkdir
+from os import mkdir, remove
 from shutil import copyfile, rmtree
 from os.path import exists, abspath, dirname
 from sys import path, argv
-from importlib.machinery import SourceFileLoader
-
 
 class UnitTestMain(unittest.TestCase):
     @staticmethod
     def setup():
-        # setup env
+        # prepare test enviroment
         current_dir = dirname(abspath(argv[0]))
         pwd_list = current_dir.split("\\")
         repo_dir = "\\".join(pwd_list[:-2])
         target_dir = repo_dir + "\\target"
         test_dir = target_dir + "\\test"
+        src_dir =  repo_dir + "\\main\\src" 
+
         # make target/test folder in repository
         if not exists(target_dir):
             mkdir(target_dir)
             mkdir(test_dir)
         elif not exists(test_dir):
             mkdir(test_dir)
+        
+        copyfile(src_dir + "\\main.py", repo_dir + "\\main\\test\\main.py")
 
         return repo_dir
 
     def test_convert_image(self):
+        # path variables
         repo_dir = UnitTestMain.setup()
-        
-        # variables
         input_image = repo_dir + "\\main\\test\\image.jpg"
         output_dir = repo_dir + "\\target\\test"
         new_name = "image_grayscale"
         expected_image = output_dir + "\\" + new_name + ".jpg"
 
         # call main function
-        main = SourceFileLoader("module.name", repo_dir + "\\main\\src\\main.py").load_module()
+        import main
         output_image = main.convert_image(input_image, output_dir, new_name)
 
         # check existance of grayscale image
@@ -48,8 +49,8 @@ class UnitTestMain(unittest.TestCase):
         self.assertTrue(test_result)
 
         # delete cache files
-        pycache_dir = repo_dir + "\\main\\src\\__pycache__"
-        rmtree(pycache_dir)
+        rmtree(repo_dir + "\\main\\test\\__pycache__")
+        remove(repo_dir + "\\main\\test\\main.py")
 
 if __name__ == "__main__":
     unittest.main()
