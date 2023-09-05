5cad6550b7f24ec5bc1f3f631cac91087693b77b
cornelcristea <petrucornel.cristea@gmail.com>
2023-09-05 11:10:47 +0300

    update separator

--- main/test/main_test.py
+++ main/test/main_test.py
@@ -12,9 +12,9 @@ class UnitTestMain(unittest.TestCase):
         # prepare test enviroment
         current_dir = dirname(abspath(argv[0]))
         pwd_list = current_dir.split(sep)
-        repo_dir = "\\".join(pwd_list[:-2])
-        target_dir = repo_dir + "\\target"
-        test_dir = target_dir + "\\test"
+        repo_dir = sep.join(pwd_list[:-2])
+        target_dir = repo_dir + sep + "target"
+        test_dir = target_dir + sep + "test"
 
         # make target/test folder in repository
         if not exists(target_dir):
@@ -29,13 +29,13 @@ class UnitTestMain(unittest.TestCase):
     def test_convert_image(self):
         # path variables
         repo_dir = UnitTestMain.setup()
-        input_image = repo_dir + "\\main\\test\\image.jpg"
-        output_dir = repo_dir + "\\target\\test"
+        input_image = repo_dir + sep + "main" + sep + "test" + sep + "image.jpg"
+        output_dir = repo_dir + sep + "targe" + sep + "test"
         new_name = "image_grayscale"
-        expected_image = output_dir + "\\" + new_name + ".jpg"
+        expected_image = output_dir + sep + new_name + ".jpg"
 
         # call main function
-        spec = spec_from_file_location("convert_image", repo_dir + "\\main\\src\\main.py")
+        spec = spec_from_file_location("convert_image", repo_dir + sep + "main" + sep + "src" + sep + "main.py")
         main = module_from_spec(spec)
         spec.loader.exec_module(main)
         output_image = main.convert_image(input_image, output_dir, new_name)
@@ -50,7 +50,7 @@ class UnitTestMain(unittest.TestCase):
         self.assertTrue(test_result)
 
         # delete cache files
-        pycache_dir = repo_dir + "\\main\\src\\__pycache__"
+        pycache_dir = repo_dir + sep + "main" + sep + "src" + sep + "__pycache__"
         rmtree(pycache_dir)
 
 if __name__ == "__main__":
