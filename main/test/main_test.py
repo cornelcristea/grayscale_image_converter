d0a506271685f5f3679f70092e8a12f09251bf0e
cornelcristea <petrucornel.cristea@gmail.com>
2023-09-05 09:51:53 +0300

    add __init__.py

--- main/test/main_test.py
+++ main/test/main_test.py
@@ -1,44 +1,41 @@
 
 import unittest
-from os import mkdir 
-from shutil import rmtree
+from os import mkdir
+from shutil import copyfile, rmtree
 from os.path import exists, abspath, dirname
 from sys import path, argv
+from importlib.machinery import SourceFileLoader
+
 
 class UnitTestMain(unittest.TestCase):
     @staticmethod
     def setup():
-        # prepare test enviroment
+        # setup env
         current_dir = dirname(abspath(argv[0]))
         pwd_list = current_dir.split("\\")
         repo_dir = "\\".join(pwd_list[:-2])
         target_dir = repo_dir + "\\target"
         test_dir = target_dir + "\\test"
-        src_dir =  repo_dir + "\\main\\src" 
-
-        # import source file as globally
-        path.append(src_dir)
-        import main
-        globals()['main'] = main
-
         # make target/test folder in repository
         if not exists(target_dir):
             mkdir(target_dir)
             mkdir(test_dir)
         elif not exists(test_dir):
             mkdir(test_dir)
-            
-        return repo_dir, main
+
+        return repo_dir
 
     def test_convert_image(self):
-        # path variables
-        repo_dir, main = UnitTestMain.setup()
+        repo_dir = UnitTestMain.setup()
+        
+        # variables
         input_image = repo_dir + "\\main\\test\\image.jpg"
         output_dir = repo_dir + "\\target\\test"
         new_name = "image_grayscale"
         expected_image = output_dir + "\\" + new_name + ".jpg"
 
         # call main function
+        main = SourceFileLoader("module.name", repo_dir + "\\main\\src\\main.py").load_module()
         output_image = main.convert_image(input_image, output_dir, new_name)
 
         # check existance of grayscale image
